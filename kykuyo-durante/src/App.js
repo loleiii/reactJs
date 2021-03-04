23bee6604defe7bd5bc438b2c74ad20cdba20164
Lorenzo Durante <lolodurante2003@gmail.com>
2021-03-04 19:06:34 -0300

    Update App.js

--- kykuyo-durante/src/App.js
+++ kykuyo-durante/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Kykuyo <code>Ecommerce</code>de alimentos organicos
+          Kykuyo <code>Ecommerce</code>de alimentos organicos!
         </p>
         <a
           className="App-link"
